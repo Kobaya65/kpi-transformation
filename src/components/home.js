9a653a659c3e5281ede683c9b6e6588acd8d8ef2
Philippe AMICE <kobaya65@free.fr>
2020-04-14 14:26:03 +0200

    création de la page d'accueil home.js, suppression de tableCell.js devenu inutile

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  
+  render() {
+    
+    return (
+      <div className="userList">
+        <h2>Bienvenue sur KPI Transformation !</h2>
+      </div>
+    )
+  }
+}
