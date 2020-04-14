917ee695fe095f2d819b71e923b24a73f0532a3a
Philippe AMICE <kobaya65@free.fr>
2020-04-14 14:35:00 +0200

    quelques améliorations css nettoyage de lignes commmentées travail sur les Link/route

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import ApplicationsList from "./components/applications";
-import Application from "./components/application";
+import Home from "./components/home";
+import ApplicationsList from "./components/applicationsList";
 import ApplicationsResp from "./components/applicationsResp";
 import ApplicationResp from "./components/applicationResp";
 
 class App extends Component {
-    
+  
   render() {
-    console.log( 'Front end started at ' + Date() );
+  console.log( 'App started at ' + Date() );
   return (
     <Router>
       <div className="render">
-        <nav className="navbar sticky-top flex-md-nowrap app-header menu-display">
+        <nav className="navbar sticky-top flex-md-nowrap app-header header-display double-border">
           <div className="col-md-2">
             <a href="/">
               <img src={`${ process.env.PUBLIC_URL }/images/soc102c.png`} height="30em" className="d-inline-block align-top" alt="logo Société Générale" />
@@ -31,7 +31,7 @@ class App extends Component {
                 <Link to="/applications" className="style-color-accomsg">Applications</Link>
               </li>
               <li>
-                <Link to="/application" className="style-color-accomsg">Application by Id</Link>
+                <Link to="/applicationById/"  className="style-color-accomsg">Application by Id</Link>
               </li>
             </ul>
           </div>
@@ -49,30 +49,14 @@ class App extends Component {
         </nav>
 
         <div className="container-fluid">
-          <Route path="/" exact component={ApplicationsList} />
-          <Route path="/applications" exact  component={ApplicationsList} />
-          <Route path="/application" exact component={Application} />
-          <Route path="/applicationsResp" exact component={ApplicationsResp} />
-          <Route path="/applicationResp" exact component={ApplicationResp} />
-          
-          {/* <h1> Titre 1</h1>
-          <p>paragraphe 1</p>
-          <h1>Titre 2</h1>
-          <p>paragraphe 1</p>
-          <p>paragraphe 2</p>
-          <h1>Titre 3</h1>
-          <p>paragraphe 1</p>
-          <h1>Titre 4</h1>
-          <p>paragraphe 1</p>
-          <h1>Titre 5</h1>
-          <p>paragraphe 1</p>
-          <h1>Titre 6</h1>
-          <p>paragraphe 1</p>
-          <h1>Titre 7</h1>
-          <p>paragraphe 1</p> */}
+          <Route exact path="/" component={Home} />
+          <Route rdexact path="/applications" component={ApplicationsList} />
+          {/* <Route exact path="/application/" component={applicationById} /> */}
+          <Route exact path="/applicationsResp" component={ApplicationsResp} />
+          <Route exact path="/applicationResp" component={ApplicationResp} />
         </div>
 
-        <footer className="footer menu-display">
+        <footer className="footer header-display simple-border">
           <p>Pied de page</p>
         </footer>
       </div>
