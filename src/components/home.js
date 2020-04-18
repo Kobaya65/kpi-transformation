771fa97e0a0a409f6b1ddb34b3a69efd3cf7ff0c
Philippe AMICE <kobaya65@free.fr>
2020-04-18 18:42:25 +0200

    centrage de Home

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     
     return (
-      <div className="userList">
+      <div className="center-all">
         <h2>Bienvenue sur KPI Transformation !</h2>
       </div>
     )
