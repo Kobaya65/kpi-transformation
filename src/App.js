9697a3ce29daa4409c70b2e436306f7e3eda5b60
Philippe AMICE <kobaya65@free.fr>
2020-04-21 11:00:26 +0200

    test sur les Link/Route

--- src/App.js
+++ src/App.js
@@ -12,57 +12,57 @@ import ApplicationRespById from "./components/applicationRespById";
 class App extends Component {
   
   render() {
-  console.log( 'App started at ' + Date() );
-  return (
-    <Router>
-      <div className="render">
-        <nav className="navbar sticky-top flex-md-nowrap app-header header-display double-border">
-          <div className="col-md-2">
-            <a href="/">
-              <img src={`${ process.env.PUBLIC_URL }/images/soc102c.png`} height="30em" className="d-inline-block align-top" alt="logo Société Générale" />
-            </a>
-          </div>
-          <div className="col-md-3 p-2">
-            KPI Transformation
+    console.log( 'App started at ' + Date() );
+    return (
+      <Router>
+        <div className="render">
+          <nav className="navbar sticky-top flex-md-nowrap app-header header-display double-border">
+            <div className="col-md-2">
+              <a href="/">
+                <img src={`${ process.env.PUBLIC_URL }/images/soc102c.png`} height="30em" className="d-inline-block align-top" alt="logo Société Générale" />
+              </a>
+            </div>
+            <div className="col-md-3 p-2">
+              KPI Transformation
+            </div>
+
+            <div className="col-md-3">
+              <ul className="nav navbar-nav">
+                <li>
+                  <Link to="/applications" className="style-color-accomsg">Applications</Link>
+                </li>
+                <li>
+                  <Link to="/applications/:_id"  className="style-color-accomsg">Application by Id</Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-md-3">
+              <ul className="nav navbar-nav">
+                <li>
+                  <Link to="/applicationsResp" className="style-color-accomsg">Applications Resp.</Link>
+                </li>
+                <li>
+                  <Link to="/applicationRespById" className="style-color-accomsg">Appli. Resp. by Id</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <div className="container-fluid">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/applications" component={ApplicationsList} />
+            <Route path="/applications/:_id" component={ApplicationById} />
+            <Route exact path="/applicationsResp" component={ApplicationsResp} />
+            <Route path="/applicationsResp" component={ApplicationRespById} />
           </div>
 
-          <div className="col-md-3">
-            <ul className="nav navbar-nav">
-              <li>
-                <Link to="/applications" className="style-color-accomsg">Applications</Link>
-              </li>
-              <li>
-                <Link to="/applications"  className="style-color-accomsg">Application by Id</Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-md-3">
-            <ul className="nav navbar-nav">
-              <li>
-                <Link to="/applicationsResp" className="style-color-accomsg">Applications Resp.</Link>
-              </li>
-              <li>
-                <Link to="/applicationRespById" className="style-color-accomsg">Appli. Resp. by Id</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-        <div className="container-fluid">
-          <Route exact path="/" component={Home} />
-          <Route exact path="/applications" component={ApplicationsList} />
-          <Route path="/applications" component={ApplicationById} />
-          <Route exact path="/applicationsResp" component={ApplicationsResp} />
-          <Route path="/applicationsResp" component={ApplicationRespById} />
+          <footer className="footer header-display simple-border">
+            <p className="center-v">Pied de page</p>
+          </footer>
         </div>
-
-        <footer className="footer header-display simple-border">
-          <p className="center-v">Pied de page</p>
-        </footer>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
   }
 }
 
