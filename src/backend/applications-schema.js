df02cf3b8e6c00f7fb27e9a5422d6b9f753d034b
Philippe AMICE <kobaya65@free.fr>
2020-04-30 15:24:39 +0200

    ajout de la collection adéquate dans le schéma

--- src/backend/applications-schema.js
+++ src/backend/applications-schema.js
@@ -1,24 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let applicationsSchema = new Schema( {
-	TechnicalIdHexa: String,
-	Authentification: String,
-	CurrentState: String,
-	Global_Id: String,
-	Commentaire: String,
-	LibelleCourt: String,
-	NomCourt: String,
-	TypeAppli: String,
-	Concepts: [
-		{
-			nom: String
-		}
-	],
-	DateDebutProd: Date,
-	DateFinProd: Date
-});
+let applicationsSchema = new Schema(
+	{
+		TechnicalIdHexa: String,
+		Authentification: String,
+		CurrentState: String,
+		Global_Id: String,
+		Commentaire: String,
+		LibelleCourt: String,
+		NomCourt: String,
+		TypeAppli: String,
+		Concepts: [
+			{
+				nom: String
+			}
+		],
+		DateDebutProd: Date,
+		DateFinProd: Date
+	},
+	{
+		collection: 'applications'
+	}
+);
 
-
-// appliquer le schéma défini à la bonne  collection
 module.exports = mongoose.model( 'applications', applicationsSchema );
--- src/backend/applicationsResp-schema.js
+++ src/backend/applicationsResp-schema.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let applicationsRespSchema = new Schema( {
-	_id: String,
-	global_id: String,
-	assignations: [
-		{
-			personne: String,
-			id_personne: String,
-			structure: String,
-			id_structure: String,
-			role: String,
-			id_role: String
-		}
-	]
-} );
+let applicationsRespSchema = new Schema(
+	{
+		id: String,
+		global_id: String,
+		assignations: [
+			{
+				personne: String,
+				id_personne: String,
+				structure: String,
+				id_structure: String,
+				role: String,
+				id_role: String
+			}
+		]
+	},
+	{
+		collection: 'applicationsResp'
+	}
+);
 
-// appliquer le schéma défini à la bonne collection
 module.exports = mongoose.model( 'applicationsResp', applicationsRespSchema );
