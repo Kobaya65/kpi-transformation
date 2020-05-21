9555320da156a4271ab5da945654a8f59147204e
Philippe AMICE <kobaya65@free.fr>
2020-05-21 19:43:06 +0200

    nouveaux statuts d'application

--- src/components/etatAppli.js
+++ src/components/etatAppli.js
@@ -2,17 +2,34 @@ import React from "react";
 
 // icônes issues de feathericons.com
 import activity from "../images/activity.svg";
-import decomm from "../images/x-octagon.svg";
-/* statuts disponibles dans Kear
-  En développement  tool.svg
-  A l'étude         search.svg
-  Fermée            
-  Installation      grid.svg
-*/
+import decomm from "../images/x.svg";
+import closed from "../images/x-octagon.svg";
+import develop from "../images/tool.svg";
+import study from "../images/search.svg";
+import install from "../images/grid.svg";
+
 const EtatAppli = (props) => {
   let imageLogo;
 
-  props.etat === "En prod" ? (imageLogo = activity) : (imageLogo = decomm);
+  switch (props.etat) {
+    case "A l'étude":
+      imageLogo = study;
+      break;
+    case "En développement":
+      imageLogo = develop;
+      break;
+    case "Installation":
+      imageLogo = install;
+      break;
+    case "En prod":
+      imageLogo = activity;
+      break;
+    case "Décommissionné":
+      imageLogo = decomm;
+      break;
+    case "Fermé":
+      imageLogo = closed;
+  }
 
   return <img src={imageLogo} alt={props.etat} height={"20px"} />;
 };
