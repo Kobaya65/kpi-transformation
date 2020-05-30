abde8a9f9061f32203b1fc089228f419da552aa6
Philippe AMICE <kobaya65@free.fr>
2020-05-30 10:01:12 +0200

    désactivation des imports axios et chart inutiles pour l'instant

--- src/containers/statParStatut.js
+++ src/containers/statParStatut.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
-import Chart from "chart.js";
+// import axios from "axios";
+// import Chart from "chart.js";
 
 import BandeauTitre from "../components/bandeau-titre";
 
