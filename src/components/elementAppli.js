8254ae8058bbac8a008fc96df879fd973b079293
Philippe AMICE <kobaya65@free.fr>
2020-06-01 14:56:25 +0200

    constructeur inutile

--- src/components/elementAppli.js
+++ src/components/elementAppli.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import { valeurNonSpecifie } from "./fonctions";
 
 export default class ElementAppli extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="col">
