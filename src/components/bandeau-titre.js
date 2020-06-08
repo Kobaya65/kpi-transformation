047a7947dad07e8120f67f364c2a73198726e8c7
Philippe AMICE <kobaya65@free.fr>
2020-06-08 09:53:29 +0200

    simplification

--- src/components/bandeau-titre.js
+++ src/components/bandeau-titre.js
@@ -3,11 +3,7 @@ import React from "react";
  * @param {*} props.titre     texte à afficher dans le bandeau
  */
 const BandeauTitre = (props) => {
-  return (
-    <div className="d-flex">
-      <h5 className="bandeau-titre p-2 flex-grow-1">{props.titre}</h5>
-    </div>
-  );
+  return <h5 className="bandeau-titre p-2 flex-grow-1">{props.titre}</h5>;
 };
 
 export default BandeauTitre;
