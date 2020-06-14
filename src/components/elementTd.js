822e972c31e745f24998663a2f63c4ad1475f46a
Philippe AMICE <kobaya65@free.fr>
2020-06-14 16:31:36 +0200

    affichage de l'icône alert-triangle en cas de donnée absente ; suppression de la classe css border-red désormais inutile

--- src/components/elementTd.js
+++ src/components/elementTd.js
@@ -1,8 +1,20 @@
 import React from "react";
+import alert from "../images/alert-triangle.svg";
 
 const ElementTd = (props) => {
-  return (
-    <td className={props.elem === "" ? "border-red" : ""}>{props.elem}</td>
+  return props.elem === "" ? (
+    <td>
+      {/* <td style={{ textAlign: "center" }}> */}
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <img
+        src={alert}
+        title={"missing data"}
+        alt={"missing data"}
+        height={"30px"}
+      />
+    </td>
+  ) : (
+    <td>{props.elem}</td>
   );
 };
 
