171eae360f7c9c81354666f3ea96d06a888bcd1e
Philippe AMICE <kobaya65@free.fr>
2020-06-17 10:11:52 +0200

    jsdoc + erreur typo sur le nom de classe css

--- src/components/elementAppli.js
+++ src/components/elementAppli.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
+/**
+ *
+ * @param {*} props value the value of the element
+ */
 export default class ElementAppli extends Component {
   render() {
     return (
       <div className="col">
         <p className="label-bold-mgl5">{this.props.label}</p>
         <div
-          className={this.props.value === "" ? "frame mising-value" : "frame"}
+          className={this.props.value === "" ? "frame missing-value" : "frame"}
         >
           {this.props.value === "" ? "non spécifié" : this.props.value}
         </div>
