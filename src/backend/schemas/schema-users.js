5b872f9485cc5b32be8c897f6635479f9c9fc754
Philippe AMICE <kobaya65@free.fr>
2020-07-01 14:49:50 +0200

    ajout du module mongoose-unique-validator

--- src/backend/schemas/schema-users.js
+++ src/backend/schemas/schema-users.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
 
-let usersSchema = new Schema({
-  matricule: String,
-  role: String,
-  pwd: String,
+const usersSchema = mongoose.Schema({
+  matricule: { type: String, required: true, unique: true },
+  role: { type: String, required: true },
+  pwd: { type: String, required: true },
 });
+
+usersSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("users", usersSchema);
