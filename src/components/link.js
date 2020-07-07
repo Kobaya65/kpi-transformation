3a95e5393a8b3bbd6655bfebb14461ff17515495
Philippe AMICE <kobaya65@free.fr>
2020-07-07 20:34:16 +0200

    jsdoc

--- src/components/link.js
+++ src/components/link.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+/**
+ * Item of the menu
+ *
+ * @param {*} props.nom  name displayed for the link
+ * @param {*} props.vers componant to be displayed
+ */
 const MyLink = (props) => (
   <Link className="dropdown-item" to={props.vers}>
     {props.nom}
