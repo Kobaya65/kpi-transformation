ba9cd0ec8fb8f8716d5d33f2e442e1006486640f
Philippe AMICE <kobaya65@free.fr>
2020-07-07 20:33:59 +0200

    modif jsdoc

--- src/components/headband.js
+++ src/components/headband.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 /**
- * @param {*} props.title     text to display in the headband
+ * @param {*} props.title  text to display in the headband
  */
 const HeadBand = (props) => {
   return <h5 className="headband p-2 flex-grow-1">{props.title}</h5>;
