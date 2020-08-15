b8c43f3d29890a7dd4c28110ee9461cff6564585
Philippe AMICE <kobaya65@free.fr>
2020-08-15 21:31:24 +0200

    composant graphique (pie, line, etc.)

---
+++ src/components/myChart.js
@@ -0,0 +1,26 @@
+import Chart from "chart.js";
+import React, { useEffect } from "react";
+
+/**
+ * create a graph based on the data provided
+ *
+ * from https://dev.to/narendersaini32/how-to-create-charts-using-chart-js-with-react-4c4n
+ *
+ * props.chartType  type of chart (line, bar, pie, etc.)
+ * props.data       data with the right structure (see https://www.chartjs.org/docs/latest/)
+ */
+export default function MyChart(props) {
+  useEffect(() => {
+    const ctx = document.getElementById("myChart");
+    new Chart(ctx, {
+      type: props.chartType,
+      data: props.data,
+    });
+  });
+
+  return (
+    <div className="App">
+      <canvas id="myChart" />
+    </div>
+  );
+}
