1cb9e0f944caea97a27a772bdb291512d132209e
Philippe AMICE <kobaya65@free.fr>
2020-08-15 21:32:40 +0200

    composant pour les statistiques par type d'application

--- src/containers/statByType.js
+++ src/containers/statByType.js
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
-// import axios from "axios";
-// import Chart from "chart.js";
+import axios from "axios";
 
 import HeadBand from "../components/headband";
+import MyChart from "../components/myChart";
+import { createDataset } from "../components/utils";
 
 export default class StatByType extends Component {
   constructor(props) {
     super(props);
     this.state = {
       url: this.props.match.url,
-      appli: {},
+      data: [],
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000" + this.state.url)
+      .then((response) => {
+        this.setState({ data: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
     return (
       <div className="container-fluid">
         <HeadBand title={"Répartition des applications par type"} />
-        <div className="row"></div>
+        <MyChart
+          chartType="pie"
+          data={createDataset(this.state.data)}
+          width="200"
+          heigth="200"
+        />
       </div>
     );
   }
