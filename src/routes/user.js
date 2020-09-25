ee60e425edbd3ef908dc689bb75d8d31b9ece3ad
Philippe AMICE <kobaya65@free.fr>
2020-09-25 10:21:57 +0200

    suppression de fichiers inutiles

--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const routerCtrl = express.Router();
-
-const userCtrl = require("../backend/controllers/user");
-
-routerCtrl.post("/signup", userCtrl.signup);
-routerCtrl.post("/login", userCtrl.login);
-
-module.exports = routerCtrl;
